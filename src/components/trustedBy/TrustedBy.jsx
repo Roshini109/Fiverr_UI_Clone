e2f475371993d3d663909ee81e7f2bb5047badb0
Roshini Dias <jbrnd21@gmail.com>
2023-09-07 03:14:12 +0530

    Create TrustedBy section

---
+++ src/components/trustedBy/TrustedBy.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './trustedby.scss'
+
+export default function TrustedBy() {
+  return (
+    <div>
+      <div className="trustedBy">
+        <div className="container">
+          <span>Trusted By:</span>
+          <img src="/assets/facebook.png" alt="" />
+          <img src="/assets/instagram.png" alt="" />
+          <img src="/assets/linkedin.png" alt="" />
+          <img src="/assets/pinterest.png" alt="" />
+          <img src="/assets/twitter.png" alt="" />
+        </div>
+      </div>
+    </div>
+  )
+}
