9aa6347dcef14cdb39588feb174a480a49525134
Roshini Dias <jbrnd21@gmail.com>
2023-09-06 13:53:29 +0530

    Create Featured Section

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './featured.scss'
+
+export default function Featured() {
+  return (
+    <div className="featured">
+        <div className="container">
+            <div className="left">
+                <h1>Find the perfect <i>freelance</i> services for your business</h1>
+                <div className="search">
+                    <div className="searchInput">
+                        <img src="/assets/search.png" alt="" />
+                        <input type="text" placeholder='Try "building mobil app"' />
+                    </div>
+                    <button>Search</button>
+                </div>
+                <div className="popular">
+                    <span>Popular :</span>
+                    <button>Web Design</button>
+                    <button>Wordpress</button>
+                    <button>Logo Design</button>
+                    <button>AI Services</button>
+                </div>
+            </div>
+            <div className="right">
+                <img src="/assets/man.png" alt="" />
+            </div>
+        </div>
+    </div>
+  )
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './home.scss'
+import Featured from '../../components/featured/Featured'
 
 export default function Home() {
   return (
     <div>
-      Home
+      <Featured/>
     </div>
   )
 }
