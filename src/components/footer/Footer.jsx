b62b80b47af3960703395a3a50377532cb7dd823
Roshini Dias <jbrnd21@gmail.com>
2023-09-15 09:58:18 +0530

    finish Home page

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,114 @@
 import React from 'react'
 import './footer.scss'
+import Logo from '../../components/logo/Logo'
 
 export default function Footer() {
   return (
-    <div>
-      Footer
+    <div className="footer">
+      <div className="container">
+        <div className="top">
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+        </div>
+
+        <hr />
+        <div className="bottom">
+          <div className="left">
+            <Logo/>
+            <span> Fiverr International Ltd. 2023</span>
+          </div>
+          <div className="right">
+            <div className="social">
+              <img src="/assets/facebook.png" alt="" />
+              <img src="/assets/instagram.png" alt="" />
+              <img src="/assets/linkedin.png" alt="" />
+              <img src="/assets/pinterest.png" alt="" />
+              <img src="/assets/twitter.png" alt="" />
+            </div>
+            <div className="link">
+              <img src="/assets/language.png" alt="" />
+              <span>English</span>
+            </div>
+            <div className="link">
+              <img src="/assets/coin.png" alt="" />
+              <span>USD</span>
+            </div>
+            <img src="/assets/accessibility.png" alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
